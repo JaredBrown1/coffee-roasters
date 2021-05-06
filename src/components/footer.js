35d413c7ca29a6586d34c7e3adbaef9c29130c55
Jared Brown <jaredbrown1@hotmail.com>
2021-05-05 19:20:55 -0700

    basic component layout

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div>
+			<div>Logo</div>
+			<div>
+				<ul>
+					<li>Home</li>
+					<li>Aboute</li>
+					<li>Create Your Plan</li>
+				</ul>
+			</div>
+			<div>Social media logos</div>
+		</div>
+	);
+};
+
+export default Footer;
