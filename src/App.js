e46e7e47187f6a1b3918e9154ee2f3c35b10cbb3
Jared Brown <jaredbrown1@hotmail.com>
2021-05-10 13:41:48 -0700

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 
+import { Route } from "react-router-dom";
+
 import HomePage from "./components/home-page/homePage";
+import AboutPage from "./components/about-page/aboutPage";
 import NavBar from "./components/nav-bar/navBar";
 import Footer from "./components/footer/footer";
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
-			<HomePage />
+			<Route exact path="/" component={HomePage} />
+			<Route path="/about" component={AboutPage} />
 			<Footer />
 		</div>
 	);
